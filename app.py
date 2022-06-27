937815706797a06da96fe29a6c4d43e39d290ed7
MDBcode <benkovic.md@gmail.com>
2022-06-28 00:35:37 +0200

    dodano nekoliko css stilova + nova struktura projektnog direktorija

--- app.py
+++ app.py
@@ -27,10 +27,18 @@ def uploadFile():
         img_full_path = os.path.join(UPLOAD_FOLDER, filename)
         uploaded_img.save(img_full_path)
         img_rel_path = "uploads/" + filename
-        if selected_method == "1":  # tu vjerojatno ide drugačije
+        if selected_method == "1":  # TODO cGAN
             result = main.predict_cgan(filename)
         elif selected_method == "2":
             result = main.predict_pix2pix(os.path.join(UPLOAD_FOLDER, filename))
+
+            formatted = (result * 255 / np.max(result)).astype('uint8')
+            img_result = Image.fromarray(formatted)
+            data = io.BytesIO()
+            img_result.save(data, "PNG")
+            encoded_img_data = base64.b64encode(data.getvalue())
+        elif selected_method == "3":
+            result = main.predict_cycleGAN(os.path.join(UPLOAD_FOLDER, filename))
 
             formatted = (result * 255 / np.max(result)).astype('uint8')
             img_result = Image.fromarray(formatted)
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from keras.utils import img_to_array
 from keras.utils import load_img
 from numpy import expand_dims
 
-PIX2PIX_MODEL_PATH = "models/model_054800.h5"
-CGAN_MODEL_PATH = "models/"  # dodati model
+PIX2PIX_MODEL_PATH = "models/saved_models/pix2pix_model_054800.h5"
+CYCLEGAN_MODEL_PATH = "models/saved_models/..."  # dodati model
+CGAN_MODEL_PATH = "models/saved_models/..."  # dodati model
 
 model_pix2pix = load_model(PIX2PIX_MODEL_PATH)
+# model_cycleGAN = load_model(CYCLEGAN_MODEL_PATH)
 # model_cgan = load_model(CGAN_MODEL_PATH)
 
 def load_image(filename):
@@ -16,13 +18,17 @@ def load_image(filename):
     pixels = expand_dims(pixels, 0)
     return pixels
 
-
 def predict_cgan(input_img):
     pass
-
 
 def predict_pix2pix(filename):
     img = load_image(filename)
     gen_image = model_pix2pix.predict(img)
     gen_image = (gen_image + 1) / 2.0
     return gen_image[0]
+
+def predict_cycleGAN(filename):
+    img = load_image(filename)
+    gen_image = model_cycleGAN.predict(img)
+    gen_image = (gen_image + 1) / 2.0  # promijeniti
+    return gen_image[0]  # promijeniti

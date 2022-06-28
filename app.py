ff6d75e40426b49ea892cfbca840a1592f4cc5be
MDBcode <benkovic.md@gmail.com>
2022-06-28 20:20:04 +0200

    spajanje cgan modela s aplikacijom + refactoring + requirements.txt

--- app.py
+++ app.py
@@ -27,37 +27,31 @@ def uploadFile():
         img_full_path = os.path.join(UPLOAD_FOLDER, filename)
         uploaded_img.save(img_full_path)
         img_rel_path = "uploads/" + filename
-        if selected_method == "1":  # TODO cGAN
-            result = main.predict_cgan(filename)
-            return
+        if selected_method == "1":
+            result = main.predict_cgan(img_full_path)
+            result = format_image(result)
+            return render_template("result_pix2pix.html", map=result, satellite=img_rel_path)
         elif selected_method == "2":
-            result = main.predict_pix2pix(os.path.join(UPLOAD_FOLDER, filename))
-            formatted = (result * 255 / np.max(result)).astype('uint8')
-            img_result = Image.fromarray(formatted)
-            data = io.BytesIO()
-            img_result.save(data, "PNG")
-            encoded_img_data = base64.b64encode(data.getvalue())
-            return render_template("result_pix2pix.html", map=encoded_img_data.decode('utf-8'), satellite=img_rel_path)
+            result = main.predict_pix2pix(img_full_path)
+            result = format_image(result)
+            return render_template("result_pix2pix.html", map=result, satellite=img_rel_path)
         elif selected_method == "3":
-            result = main.predict_cycleGAN_sat_to_map(os.path.join(UPLOAD_FOLDER, filename))
-            result = result.reshape((256,256,3))
-            formatted = (result * 255 / np.max(result)).astype('uint8')
-            img_result = Image.fromarray(formatted)
-            data = io.BytesIO()
-            img_result.save(data, "PNG")
-            encoded_img_data = base64.b64encode(data.getvalue())
-            return render_template("result_cycleGAN.html", map=encoded_img_data.decode('utf-8'), satellite=img_rel_path)
+            result = main.predict_cycleGAN_sat_to_map(img_full_path)
+            result = format_image(result)
+            return render_template("result_cycleGAN.html", map=result, satellite=img_rel_path)
         elif selected_method == "4":
-            result = main.predict_cycleGAN_map_to_sat(os.path.join(UPLOAD_FOLDER, filename))
-            result = result.reshape((256,256,3))
-            formatted = (result * 255 / np.max(result)).astype('uint8')
-            img_result = Image.fromarray(formatted)
-            data = io.BytesIO()
-            img_result.save(data, "PNG")
-            encoded_img_data = base64.b64encode(data.getvalue())
-            return render_template("result_cycleGAN2.html", map=img_rel_path, satellite=encoded_img_data.decode('utf-8'))
+            result = main.predict_cycleGAN_map_to_sat(img_full_path)
+            result = format_image(result)
+            return render_template("result_cycleGAN2.html", map=img_rel_path, satellite=result)
     return home()
 
+def format_image(img):
+    formatted = (img * 255 / np.max(img)).astype('uint8')
+    img_result = Image.fromarray(formatted)
+    data = io.BytesIO()
+    img_result.save(data, "PNG")
+    encoded_img_data = base64.b64encode(data.getvalue())
+    return encoded_img_data.decode('utf-8')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from instance_normalization import InstanceNormalization
 PIX2PIX_MODEL_PATH = "models/saved_models/pix2pix_model_054800.h5"
 CYCLEGAN_MODEL_PATH_SAT_TO_MAP = "models/saved_models/cycleGAN_model_AtoB_004384.h5"
 CYCLEGAN_MODEL_PATH_MAP_TO_SAT = "models/saved_models/cycleGAN_model_BtoA_004384.h5"
-CGAN_MODEL_PATH = "models/saved_models/..."  # dodati model
+CGAN_MODEL_PATH = "models/saved_models/our_cgan.h5"
 
 cust = {"InstanceNormalization": InstanceNormalization}
 
 model_pix2pix = load_model(PIX2PIX_MODEL_PATH)
 model_cycleGAN_sat_to_map = load_model(CYCLEGAN_MODEL_PATH_SAT_TO_MAP, cust)
 model_cycleGAN_map_to_sat = load_model(CYCLEGAN_MODEL_PATH_MAP_TO_SAT, cust)
-# model_cgan = load_model(CGAN_MODEL_PATH)
+model_cgan = load_model(CGAN_MODEL_PATH)
+
 
 def load_image(filename):
     pixels = load_img(filename)
@@ -23,23 +24,26 @@ def load_image(filename):
     pixels = expand_dims(pixels, 0)
     return pixels
 
-def predict_cgan(input_img):
-    pass
+def predict_cgan(filename):
+    img = load_image(filename)
+    predicted_img = model_cgan.predict(img)
+    predicted_img = predicted_img.reshape((256,256,3))
+    return (predicted_img + 1) / 2.0
 
 def predict_pix2pix(filename):
     img = load_image(filename)
-    gen_image = model_pix2pix.predict(img)
-    gen_image = (gen_image + 1) / 2.0
-    return gen_image[0]
+    predicted_img = model_pix2pix.predict(img)
+    predicted_img = predicted_img.reshape((256, 256, 3))
+    return (predicted_img + 1) / 2.0
 
 def predict_cycleGAN_sat_to_map(filename):
     img = load_image(filename)
     predicted_map = model_cycleGAN_sat_to_map.predict(img)
-    predicted_map = (predicted_map + 1) / 2.0
-    return predicted_map
+    predicted_map = predicted_map.reshape((256, 256, 3))
+    return (predicted_map + 1) / 2.0
 
 def predict_cycleGAN_map_to_sat(filename):
     img = load_image(filename)
     predicted_satellite = model_cycleGAN_map_to_sat.predict(img)
-    predicted_satellite = (predicted_satellite + 1) / 2.0
-    return predicted_satellite
+    predicted_satellite = predicted_satellite.reshape((256, 256, 3))
+    return (predicted_satellite + 1) / 2.0
